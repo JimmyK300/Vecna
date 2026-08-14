80428b2014014c796c1f987fac94e5e5c2656300
JimmyK300 <134896739+JimmyK300@users.noreply.github.com>
2026-08-14 21:07:30 +0700

    Register Qwen VL feature extractor

--- aic51-src/aic51/packages/analyse/features/__init__.py
+++ aic51-src/aic51/packages/analyse/features/__init__.py
@@ -2,4 +2,5 @@ from .image_clip import ImageCLIP
 from .video_clip import VideoCLIP
 from .ocr import OCR
 from .asr import ASR
+from .qwen_vl import QwenVLEmbedding
 from .feature_extractor import FeatureExtractor, FeatureExtractorFactory
